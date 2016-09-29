5186902258b503118e59ab404979e96b9b5b4db9
nazarioes <einazario@fullsail.edu>
2016-09-29 09:08:49 -0400

    DOOM 3 vanilla and ROE running through run_release.bat
    Creating new branch.  Semi-figured out what variables need to be set to
    what in order to run vanilla DOOM 3 and ROE.

--- CreateBuildFiles.bat
+++ CreateBuildFiles.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-cmake -DBOOST_ROOT:PATH=c:/local/boost -DBOOST_LIBRARYDIR:PATH=c:/local/boost/lib32-msvc-14.0 -DDHEWM3LIBS=../dhewm3-libs/i686-w64-mingw32 ./neo
+cmake -DBOOST_ROOT:PATH=d:/boost/boost_1_61_0 -DBOOST_LIBRARYDIR:PATH=d:/boost/boost_1_61_0/lib32-msvc-14.0 -DDHEWM3LIBS=../dhewm3-libs/i686-w64-mingw32 ./neo
 pause
