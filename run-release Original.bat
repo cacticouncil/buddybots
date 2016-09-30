f419d79a46deeb59ebc97a0952c8db6989e132cf
nazarioes <einazario@fullsail.edu>
2016-09-30 09:41:21 -0400

    Revert "Merge remote-tracking branch 'refs/remotes/origin/buddybots_Esteban' into buddybots_ctf"
    This reverts commit 0b7b6258c4f9bde309b9b3750f76dd79c27230d9, reversing
    changes made to b66ac1b5aaf402dc62ee81318af19e93babaa59e.

--- run-release Original.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-Release\dhewm3.exe +set fs_basepath "./" +set com_allowConsole 1 +set si_pure 0 +set fs_game buddybots +set fs_game_base d3xp
--- run-release.bat
+++ run-release.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-Release\dhewm3.exe +set fs_basepath "./" +set r_multiSamples 0 +set com_allowConsole 1 +set si_pure 0 +set fs_game d3xp fs_game_base base
+Release\dhewm3.exe +set fs_basepath "./" +set com_allowConsole 1 +set si_pure 0 +set fs_game buddybots +set fs_game_base d3xp
