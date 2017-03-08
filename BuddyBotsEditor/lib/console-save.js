4ebef8c8a678078c1f123bad17efc0e001eb1a07
Zixun Xiang <zxiang@fullsail.edu>
2017-03-08 15:55:41 -0500

    added user part

--- BuddyBotsEditor/lib/console-save.js
+++ BuddyBotsEditor/lib/console-save.js
@@ -15,8 +15,7 @@
 
         var blob = new Blob([data], { type: 'text/py' }),
             e = document.createEvent('MouseEvents'),
-            a = document.createElement('a'),
-            b = document.createElement('b')
+            a = document.createElement('a')
 
         a.download = filename
         a.href = window.URL.createObjectURL(blob)
@@ -25,3 +24,26 @@
         a.dispatchEvent(e)
     }
 })(console)
+
+function WriteToFile() {
+    var author = document.getElementById("author")
+    //if (typeof author === "object") {
+    //    author = JSON.stringify(author, undefined, 4)
+    //}
+    var ui_name = document.getElementById("ui_name")
+    //if (typeof ui_name === "object") {
+    //    ui_name = JSON.stringify(ui_name, undefined, 4)
+    //}
+    var filename = ui_name + ".def"
+
+    var blob = new Blob([author], { type: 'text/json' }),
+           e = document.createEvent('MouseEvents'),
+           a = document.createElement('a')
+
+    a.download = filename
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    a.dispatchEvent(e)
+
+}
