0a166ca6f548b7b8d06777d38a71cbe27fda75aa
jthornbeck <jthornbeck@gmail.com>
2018-10-24 19:36:00 -0400

    adding my createbuildfiles for cmake

--- CreateBuildFiles.bat
+++ CreateBuildFiles.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-cmake -DBOOST_ROOT:PATH=c:/local/boost_1_64_0 -DBOOST_LIBRARYDIR:PATH=c:/local/boost_1_64_0/lib32-msvc-14.1 -DDHEWM3LIBS=../dhewm3-libs/i686-w64-mingw32 ./neo
+cmake -DBOOST_ROOT:PATH=c:/local/boost_1_64_0 -DBOOST_LIBRARYDIR:PATH=c:/local/boost_1_64_0/lib32-msvc-14.1 -DDHEWM3LIBS=../dhewm3-libs-master/i686-w64-mingw32 ./neo
 pause
