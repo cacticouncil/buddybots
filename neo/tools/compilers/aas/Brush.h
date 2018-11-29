af1b68885bf176be9b119f2aa83f3e90a0e8f3e9
Numbuh474 <35499003+Numbuh474@users.noreply.github.com>
2018-11-28 19:59:54 -0500

    Update Brush.h

--- neo/tools/compilers/aas/Brush.h
+++ neo/tools/compilers/aas/Brush.h
@@ -31,6 +31,7 @@ If you have questions concerning this license or the applicable additional terms
 
 #include "idlib/containers/PlaneSet.h"
 #include "idlib/geometry/Winding.h"
+#include "idlib/Str.h"
 
 /*
 ===============================================================================
